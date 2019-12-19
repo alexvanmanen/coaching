a796f9079a41735e9bcf6d0a3fa2514dc6143006
scmeijers <scm15-8@hotmail.com>
2019-12-19 11:51:18 +0100

    update manager

--- src/main/java/nl/ycn/coaching/model/Manager.java
+++ src/main/java/nl/ycn/coaching/model/Manager.java
@@ -3,33 +3,20 @@ package nl.ycn.coaching.model;
 import java.util.Date;
 import java.util.List;
 
-public class Manager extends User{
-
+public class Manager extends User {
+	
 	List<Trainee> trainees;
-
-
-	public void getDashboard(Trainee){
+	
+	
+	public void getDashboard (Trainee trainee) {
 	}
-
-	public void createCalendar(){
+	
+	public void createCalendar () {
 	}
-
-	public void makeAppointment(Date, Trainee){
+	
+	public void makeAppointment (Date date, Trainee trainee) {
 	}
-
-	public void getProgress(Trainee){
+	
+	public void getProgress (Trainee trainee) {
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
