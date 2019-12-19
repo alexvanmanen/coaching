00190457ccf5b288794c0d97c50c2c3e442eb656
noobacus <vuong.ngo.anh@gmail.com>
2019-12-19 13:21:37 +0100

    personal hard/soft skill and 360

--- src/main/java/nl/ycn/coaching/model/PersonalSoftskill.java
+++ src/main/java/nl/ycn/coaching/model/PersonalSoftskill.java
@@ -1,4 +1,15 @@
 package nl.ycn.coaching.model;
 
 public class PersonalSoftskill {
+
+    /* private Softskill softskill;
+    *  private List<DateMessage> dateMessageList; */
+
+
+    private String report;
+
+    public void addDateMessage() {
+
+    }
+
 }
