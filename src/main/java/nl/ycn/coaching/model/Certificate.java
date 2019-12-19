1bdd05101e7fcfad2a013e915a7b5f04e4b1989e
Wouter <wouterabels@outlook.com>
2019-12-19 13:21:02 +0100

    Atributes added

--- src/main/java/nl/ycn/coaching/model/Certificate.java
+++ src/main/java/nl/ycn/coaching/model/Certificate.java
@@ -1,4 +1,12 @@
 package nl.ycn.coaching.model;
 
+import java.io.File;
+
 public class Certificate {
+
+    private String name;
+
+    private File certificate;
+
+    private String skillCoupling;
 }
