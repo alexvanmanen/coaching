f49ecb017854f86481e4b41cb9f576520ec5f886
Luuk Wempe <luukwempe@hotmail.com>
2019-12-19 12:04:53 +0100

    Update

--- src/main/java/nl/ycn/coaching/model/Users/TalentManager.java
+++ src/main/java/nl/ycn/coaching/model/Users/TalentManager.java
@@ -1,6 +1,10 @@
 package nl.ycn.coaching.model.Users;
 
+import nl.ycn.coaching.model.Trainee;
 import nl.ycn.coaching.model.User;
 
+import java.util.List;
+
 public class TalentManager extends User {
+	List<Trainee> traineeList;
 }
