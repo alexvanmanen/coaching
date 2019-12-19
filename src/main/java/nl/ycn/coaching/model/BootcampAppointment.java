c52c8291e01c13e69cdad117c195fb3bf071741f
scmeijers <scm15-8@hotmail.com>
2019-12-19 13:20:16 +0100

    update BootcampAppointment

--- src/main/java/nl/ycn/coaching/model/BootcampAppointment.java
+++ src/main/java/nl/ycn/coaching/model/BootcampAppointment.java
@@ -1,4 +1,11 @@
 package nl.ycn.coaching.model;
 
+import java.util.Date;
+
 public class BootcampAppointment {
+	
+	public Date date;
+	public String teacher;
+	
+	public  void makeAppointment() {}
 }
