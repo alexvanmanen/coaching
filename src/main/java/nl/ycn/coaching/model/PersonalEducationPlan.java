2de77f294a04a1e7ff1d90aafaa9d1d65d860f91
Luuk Wempe <luukwempe@hotmail.com>
2019-12-19 15:40:25 +0100

    update controller

--- src/main/java/nl/ycn/coaching/model/PersonalEducationPlan.java
+++ src/main/java/nl/ycn/coaching/model/PersonalEducationPlan.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 public class PersonalEducationPlan {
 
-    public PersonalEducationPlan(){}
-
     private List<PersonalSoftskill> personalSoftskillList;
-
     private List<PersonalHardskill> personalHardskillList;
-
     private String recommendations;
+
+    public PersonalEducationPlan(){}
 
     public List<PersonalHardskill> getPersonalHardskillList(){
         return personalHardskillList;
