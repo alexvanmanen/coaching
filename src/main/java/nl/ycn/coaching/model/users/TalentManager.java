f2b94517b3b892c6f3166b469c6d1bf12579bfda
scmeijers <scm15-8@hotmail.com>
2019-12-19 13:34:10 +0100

    update TalentManager

--- src/main/java/nl/ycn/coaching/model/users/TalentManager.java
+++ src/main/java/nl/ycn/coaching/model/users/TalentManager.java
@@ -1,9 +1,17 @@
 package nl.ycn.coaching.model.users;
 
-import nl.ycn.coaching.model.User;
+import nl.ycn.coaching.model.PersonalEducationPlan;
 
+import java.util.Date;
 import java.util.List;
 
 public class TalentManager extends AppUser {
-	List<Trainee> traineeList;
+	
+	public List<Trainee> traineeList;
+	
+	public void makeAppointment(Date date, Trainee trainee) {}
+	
+	public void getDashboard(Trainee trainee) {}
+	
+	public void makeRecommendations (PersonalEducationPlan personalEducationPlan) {}
 }
