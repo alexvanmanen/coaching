b11cd97c0f9068488863bea8a08a038fa709cc76
Luuk Wempe <luukwempe@hotmail.com>
2019-12-19 15:23:00 +0100

    update controller

--- src/main/java/nl/ycn/coaching/controller/PepController.java
+++ src/main/java/nl/ycn/coaching/controller/PepController.java
@@ -1,7 +1,21 @@
 package nl.ycn.coaching.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PepController {
+
+	@GetMapping("personalhardskillform")
+	public String getpersonalhardskillform(){
+		return "/dashboardpages/personalhardskillform";
+	}
+
+	@PostMapping("personalhardskillform")
+	public String goTohardskillform(){
+		return "/dashboardpages/personalhardskillform";
+	}
+
+
 }
