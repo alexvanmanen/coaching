00ddbef402bd94fc81df1d6a8bc07ca1b1d832b6
Wouter <wouterabels@outlook.com>
2019-12-19 11:39:57 +0100

    Atributes added

--- src/main/java/nl/ycn/coaching/controller/WebAppUser.java
+++ src/main/java/nl/ycn/coaching/controller/WebAppUser.java
@@ -1,19 +1,5 @@
 package nl.ycn.coaching.controller;
 
 public class WebAppUser {
-
-    private String name;
-
-    private String street;
-
-    private String city;
-
-    private String streetnr;
-
-    private String zipcode;
-
-    private String provence;
-
-    private String country;
 }
 
--- src/main/java/nl/ycn/coaching/model/NAWdetails.java
+++ src/main/java/nl/ycn/coaching/model/NAWdetails.java
@@ -1,4 +1,18 @@
 package nl.ycn.coaching.model;
 
 public class NAWdetails {
+
+    private String name;
+
+    private String street;
+
+    private String city;
+
+    private String streetnr;
+
+    private String zipcode;
+
+    private String provence;
+
+    private String country;
 }
