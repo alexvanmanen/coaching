e646c6c8668f5360ba6bd90b029ebb0c4dfbcc05
noobacus <vuong.ngo.anh@gmail.com>
2019-12-23 16:16:42 +0100

    add hardskills to list in pep class

--- src/main/java/nl/ycn/coaching/model/PersonalEducationPlan.java
+++ src/main/java/nl/ycn/coaching/model/PersonalEducationPlan.java
@@ -1,9 +1,15 @@
 package nl.ycn.coaching.model;
 
+import nl.ycn.coaching.database.HardskillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PersonalEducationPlan {
+
+    @Autowired
+    private HardskillRepository hardskillRepository;
 
     private List<PersonalSoftskill> personalSoftskillList;
     private List<PersonalHardskill> personalHardskillList;
@@ -20,6 +26,20 @@ public class PersonalEducationPlan {
         return personalHardskillList;
     }
 
+    /* Fill the personal Soft- and Hardskill lists given the username */
+    public void fillPersonalLists(String username) {
+        /*TODO add Softskills too*/
+
+        List<PersonalHardskill> databasePersonalHardskills = hardskillRepository.findAll();
+
+        for (PersonalHardskill entry : databasePersonalHardskills) {
+            /*TODO change getName to getUsername*/
+            if (entry.getName().equals(username)) {
+                addHardskill(entry);
+            }
+        }
+    }
+
     public void addHardskill(PersonalHardskill skill) {
         this.personalHardskillList.add(skill);
     }
@@ -27,4 +47,5 @@ public class PersonalEducationPlan {
     public void addSoftskill(PersonalSoftskill skill) {
         this.personalSoftskillList.add(skill);
     }
+
 }
